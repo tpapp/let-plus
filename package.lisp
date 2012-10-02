985db21a7c932cb240e3cb7c5981c016bfe44cce
tpapp <tkpapp@gmail.com>
2012-10-02 18:01:32 +0200

    Cleaned up export list.
    Broken up to multiple :export clauses.

--- package.lisp
+++ package.lisp
@@ -2,28 +2,37 @@
 
 (defpackage #:let-plus
   (:use #:cl #:alexandria #:anaphora)
-  (:export
-
-   ;; basic user interface
-
+  (:export ; basic user interface
    #:let+
    #:&ign
-   #:&accessors #:&accessors-r/o
-   #:&slots #:&slots-r/o
-   #:&structure #:&structure-r/o
+   #:&accessors
+   #:&accessors-r/o
+   #:&slots
+   #:&slots-r/o
+   #:&structure
+   #:&structure-r/o
    #:&values
-   #:&array-elements #:&array-elements-r/o
-   #:&flet #:&labels #:&macrolet #:&symbol-macrolet
-   #:&plist #:&plist-r/o
-   #:&hash-table #:&hash-table-r/o
-
-   ;; defining new forms
-
-   #:let+-expansion #:let+-expansion-for-list #:define-let+-expansion
-
-   ;; extensions
-
-   #:&flet+ #:&labels+
-   #:lambda+ #:defun+
-   #:define-structure-let+ #:&fwrap
-   #:&assert #:&once-only #:&with-gensyms #:&complex))
+   #:&array-elements
+   #:&array-elements-r/o
+   #:&flet #:&labels
+   #:&macrolet
+   #:&symbol-macrolet
+   #:&plist
+   #:&plist-r/o
+   #:&hash-table
+   #:&hash-table-r/o)
+  (:export ; defining new forms
+   #:let+-expansion
+   #:let+-expansion-for-list
+   #:define-let+-expansion)
+  (:export ; extensions
+   #:&flet+
+   #:&labels+
+   #:lambda+
+   #:defun+
+   #:define-structure-let+
+   #:&fwrap
+   #:&assert
+   #:&once-only
+   #:&with-gensyms
+   #:&complex))
